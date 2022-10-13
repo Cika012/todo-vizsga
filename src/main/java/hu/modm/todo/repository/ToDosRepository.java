6f977adefb36fe423dbd04b319a20713c8bf7095
modm <modmarton@any.hu>
2022-10-13 11:53:15 +0200

    #New  - ToDosRepositoryIT createToDo teszt #Fix  - ToDosRepository ToDo entity használata User helyett

--- src/main/java/hu/modm/todo/repository/ToDosRepository.java
+++ src/main/java/hu/modm/todo/repository/ToDosRepository.java
@@ -1,6 +1,6 @@
 package hu.modm.todo.repository;
-import hu.modm.todo.entity.User;
+import hu.modm.todo.entity.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ToDosRepository extends JpaRepository<User, Long> {
+public interface ToDosRepository extends JpaRepository<ToDo, Long> {
 }
---
+++ src/test/java/hu/modm/todo/repository/ToDosRepositoryIT.java
@@ -0,0 +1,49 @@
+package hu.modm.todo.repository;
+
+import hu.modm.todo.entity.ToDo;
+import hu.modm.todo.entity.User;
+import hu.modm.todo.entity.enums.Importance;
+import hu.modm.todo.entity.enums.Status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class ToDosRepositoryIT {
+
+    @Autowired
+    ToDosRepository toDosRepository;
+    @Autowired
+    UsersRepository usersRepository;
+
+
+
+    @Test
+     void createToDo() {
+        var user = new User(null, "sample@test.com");
+        usersRepository.save(user);
+
+        String expectedDescription = "create integration test";
+
+        var todo = new ToDo(null,
+                expectedDescription,
+                LocalDate.of(2022,10,14),
+                Importance.IMPORTANT,
+                Status.NOT_STARTED,
+                user);
+
+        toDosRepository.save(todo);
+
+        var todos = toDosRepository.findAll();
+        assertEquals(1, todos.size());
+        assertThat(todos).isNotNull();
+        assertThat(todos)
+                .extracting(ToDo::getDescription)
+                .contains(expectedDescription);
+    }
+}
